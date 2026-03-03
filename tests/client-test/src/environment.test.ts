09da9692f4325b6cc46b2fc0eb967c799d9714d1
Scrivener07 <ScrivenerMods@gmail.com>
2026-03-02 19:28:09 -0500

    Fixed barrel exports consumed by tests

--- tests/client-test/src/environment.test.ts
+++ tests/client-test/src/environment.test.ts
@@ -1,6 +1,6 @@
 import * as assert from "assert";
 import * as vscode from "vscode";
-import { GameDirectory } from "@soase/client/environment";
+import { GameDirectory } from "@soase/client";
 
 suite("GameDirectory", function () {
     test("isValid() returns true for a valid game directory", async function () {
